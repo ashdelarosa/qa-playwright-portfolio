79c2d47a9492a2459ce366f70edfc09f5c31ea66
ashdelarosa <maddelarosa@gmail.com>
2026-05-08 13:28:33 +0800

    Add locked out user login test

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -20,3 +20,15 @@ test('user can log in with valid credentials', async ({ page }) => {
   
     await expect(page.locator('[data-test="error"]')).toBeVisible();
   });
+
+  test('user cannot log in when account is locked out', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+  
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+  
+    await expect(page.locator('[data-test="error"]')).toContainText(
+      'Sorry, this user has been locked out.'
+    );
+  });
